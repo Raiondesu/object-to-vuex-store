b5e19a630190e1fa067ff11550a5b537e82d70f6
Raiondesu <a.raiondesu@gmail.com>
2017-10-21 23:53:35 +0300

    Upd readme. Refactoring. Got code and readme together finally.

--- index.js
+++ index.js
@@ -9,18 +9,7 @@
  * @returns valid vuex store object for passing into a Vuex constructor.
  */
 export function objectToStore(obj, namespaced = false) {
-  const filters = (function(_obj) {
-    const __desc = prop => Object.getOwnPropertyDescriptor(_obj, prop);
-    const __isValid    = prop => !!_obj && !!prop && !!__desc(prop);
-    const __isFunction = prop => typeof _obj[prop] === 'function';
-
-    return {
-      state: (prop) => __isValid(prop) && !__desc(prop).get && !__desc(prop).set && !__isFunction(prop),
-      getter: (prop) => __isValid(prop) && __desc(prop).get && !__desc(prop).set && !__isFunction(prop),
-      mutation: (prop) => __isValid(prop) && !__desc(prop).get && __desc(prop).set && !__isFunction(prop),
-      action: (prop) => __isValid(prop) && __isFunction(prop)
-    }
-  }(obj));
+  const filters = createFilters(obj);
 
   function filterObject(filter) {
     var result = {};
@@ -32,35 +21,15 @@ export function objectToStore(obj, namespaced = false) {
           break;
 
         case filters.mutation:
-          const setter = Object.getOwnPropertyDescriptor(obj, key).set;
-          result[key] = (state, payload) => setter.call(obj, payload);
+          result[key] = createMutation(obj, key);
           break;
 
         case filters.action:
-          const args = getArgs(obj[key]);
-
-          result[key] = (context, payload) => {
-            if (args.indexOf('rootState') > -1)
-              payload['rootState'] = context.rootState;
-            if (args.indexOf('rootGetters') > -1)
-              payload['rootGetters'] = context.rootGetters;
-
-            if (!obj['commit'] || !obj['dispatch']) {
-              obj['commit'] = context.commit;
-              obj['dispatch'] = context.dispatch;
-            }
-
-            return obj[key].apply(obj, isObject(payload) ? args.map(value => payload[value]) : [payload]);
-          }
+          result[key] = createAction(obj, key);
           break;
-
+          
         case filters.state: default:
-          Object.defineProperty(result, key, {
-            configurable: false,
-            enumerable: true,
-            get: () => obj[key],
-            set: value => obj[key] = value
-          });
+          createProp(result, obj, key);
           break;
       }
     }
@@ -74,6 +43,47 @@ export function objectToStore(obj, namespaced = false) {
     getters: filterObject(filters.getter),
     mutations: filterObject(filters.mutation),
     actions: filterObject(filters.action)
+  }
+}
+
+function createFilters(_obj) {
+  const __desc = prop => Object.getOwnPropertyDescriptor(_obj, prop);
+  const __isValid    = prop => !!_obj && !!prop && !!__desc(prop);
+  const __isFunction = prop => typeof _obj[prop] === 'function';
+
+  return {
+    state: (prop) => __isValid(prop) && !__desc(prop).get && !__desc(prop).set && !__isFunction(prop),
+    getter: (prop) => __isValid(prop) && __desc(prop).get && !__desc(prop).set && !__isFunction(prop),
+    mutation: (prop) => __isValid(prop) && !__desc(prop).get && __desc(prop).set && !__isFunction(prop),
+    action: (prop) => __isValid(prop) && __isFunction(prop)
+  }
+}
+
+function createProp(obj, donor, key) {
+  Object.defineProperty(obj, key, {
+    configurable: false,
+    enumerable: true,
+    get: () => donor[key],
+    set: value => donor[key] = value
+  });
+}
+
+function createMutation(obj, key) {
+  const setter = Object.getOwnPropertyDescriptor(obj, key).set;  
+  return (state, payload) => setter.call(obj, payload);
+}
+
+function createAction(obj, key) {
+  const args = getArgs(obj[key]);
+  return function (context, payload) {
+    if (!obj['commit'] || !obj['dispatch'] || !obj['rootState'] || !obj['rootGetters']) {
+      obj['commit'] = context.commit;
+      obj['dispatch'] = context.dispatch;
+      obj['rootState'] = context.rootState;
+      obj['rootGetters'] = context.rootGetters;
+    }
+
+    return obj[key].apply(obj, isObject(payload) ? args.map(value => payload[value]) : [payload]);
   }
 }
 
